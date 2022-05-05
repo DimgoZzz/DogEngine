bfc07192ea054e1a8ebd78aa7afa42b2250cd98d
Dimgo <dmgolovinzzz@gmail.com>
2022-05-05 05:29:32 +0300

    proj: add Engine part

---
+++ Public/DEngine/DEngine.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "DEngine/Engine/IEngine.h"
+
+namespace DogFW
+{
+	using namespace DogFW::engine;
+}
---
+++ Public/DEngine/DSystems.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "DEngine/Systems/File/FileSystem.h"
+#include "DEngine/Systems/Log/LogSystem.h"
+#include "DEngine/Systems/Window/WindowSystem.h"
+#include "DEngine/Systems/App/AppSystem.h"
+
+namespace DogFW
+{
+	using namespace DogFW::filesystem;
+	using namespace DogFW::logsystem;
+	using namespace DogFW::appsystem;
+	using namespace DogFW::windowsystem;
+
+}
